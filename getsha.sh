855d9d66d392f23a0e1e55d45b3782e1c501db20
Gokhan Yucegok <gyucegok@gmail.com>
2022-02-10 12:12:32 +0000

    v0.12.4

--- getsha.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CURRENT_DIR=${PWD}
-for template in $(find . -name '*.tmpl'); do envsubst < ${template} > ${template%.*}; done
-mv ${CURRENT_DIR}/templates/*.yaml ${CURRENT_DIR}/kubernetes-manifests
